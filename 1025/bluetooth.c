80be0093468a0aa11468b001ecea42c194e97cd8
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-03 16:32:34 +0900

    Update bluetooth.c

--- 1025/bluetooth.c
+++ 1025/bluetooth.c
@@ -1,24 +1,19 @@
+
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTSerial(2,3); 
+SoftwareSerial BTSerial(2, 3); // SoftwareSerial(RX, TX)
+byte buffer[1024]; // 데이터를 수신 받을 버퍼
+int bufferPosition; // 버퍼에 데이타를 저장할 때 기록할 위치
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("Hello!");
-  
-  BTSerial.begin(9600); //set the data rate for the BT
-  
+void setup(){
+  BTSerial.begin(9600); 
+  Serial.begin(9600); 
+  bufferPosition = 0; // 버퍼 위치 초기화
 }
 
-void loop()
-{
-  //BT -> Data -> Serial
-  if(BTSerial.available()) {
-    Serial.write(BTSerial.read());
-  }
-  //Serial -> Data -> BT
-  if(Serial.available()){
-    BTSerial.write(Serial.read());
-  }
+void loop(){
+  int temp = analogRead(A0);
+  Serial.println(temp);
+  BTSerial.println(temp);
+  delay(500);
 }
