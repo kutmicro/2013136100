1aecf18918ee524f16b00757692e6ea579a6883a
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-10 16:18:13 +0900

    Update DcMotor.c

--- 1025/DcMotor.c
+++ 1025/DcMotor.c
@@ -1,25 +1,105 @@
-//아두이노 핀3번이 트랜지스터를 on/off하는데 사용되어 motorPin이라고 명명
-int motorPin = 3;
-
-void setup()
-{
-  pinMode(motorPin, OUTPUT);
-  Serial.begin(9600);
-  while(! Serial);
-  Serial.println("Speed 0 to 255");
+//모터A 컨트롤
+int IN1=7;
+int IN2=6;
+ 
+//모터B 컨트롤
+int IN3=5;
+int IN4=4;
+ 
+void setup() {
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
 }
-
-//loop함수에서는 Serial.parseInt함수가 시리얼 모니터에 입력된 숫자를 스트링형태로 읽어 int 타입으로 변환
-//시리얼 모니터창에는 아무 숫자나 입력하여도 loop함수내의 if문에서 0~255사이의 값만 analogwrite합니다.
-void loop()
-{
-  if(Serial.available())
-  {
-    int speed = Serial.parseInt();
-    if (speed >= 0 && speed <= 255)
-    {
-      analogWrite(motorPin, speed);
-    }
-  }
+ 
+void loop() {
+  bothMotorStart();
+  delay(3000);
+  turnLeft();
+  delay(3000);  
+  turnRight();
+  delay(3000);
+  motorA_Rotation();
+  delay(3000);
+  motorB_Rotation();
+  delay(3000);
+  motorA_Reverse();
+  delay(3000);
+  motorB_Reverse();
+  delay(3000);
+  stopAllMotor();
+  delay(3000);
 }
-
+ 
+//모터A,B 정회전
+void bothMotorStart()
+{
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,HIGH);
+    digitalWrite(IN4,LOW);
+}
+ 
+//모터A,B Stop
+void stopAllMotor()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+}
+ 
+//모터A 역회전, 모터B 정회전
+void turnLeft()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,HIGH);
+    digitalWrite(IN3,HIGH);
+    digitalWrite(IN4,LOW);
+}
+ 
+//모터A 정회전, 모터B 역회전
+void turnRight()
+{
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,HIGH);
+}
+ 
+//모터A 정회전, 모터B Stop
+void motorA_Rotation()
+{
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+}
+ 
+//모터A Stop, 모터B 정회전
+void motorB_Rotation()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,HIGH);
+    digitalWrite(IN4,LOW);
+}
+ 
+//모터A 역회전, 모터B Stop
+void motorA_Reverse()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,HIGH);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+}
+ 
+//모터A Stop, 모터B 역회전
+void motorB_Reverse()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,HIGH);
+}
